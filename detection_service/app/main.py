24f41b9e72b62f34695d34fea3c67527ac221ca7
Fatma Amr <fatmaamr.engineer@gmail.com>
2026-09-04 15:57:00 +0300

    refactor(detection): update detection routes for dynamic statistical calculations and live attack metrics

--- detection_service/app/main.py
+++ detection_service/app/main.py
@@ -4,6 +4,7 @@ from app.config import settings
 from app.routes import router
 from app.model_handler import model_handler
 from app.kafka_consumer import kafka_consumer
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,4 +14,15 @@ async def lifespan(app: FastAPI):
     kafka_consumer.stop()
 
 app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(router)
